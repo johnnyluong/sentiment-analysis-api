abf5221d931978557a9c35ecc4449f4007cab81f
Johnny Luong <jluong.dev@gmail.com>
2022-08-26 01:38:06 -0700

    integrated vader

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
-from flask_restful import Api
-
-from resources.run_vader import runVader
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource, reqparse
+from resources.vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(runVader, "/vader/<string:content>")
+parser = reqparse.RequestParser()
+parser.add_argument('text', type=str, required=True, help='Content required.',location='form')
+# args = parser.parse_args()
+
+class SentimentAnalysis(Resource):
+    def get(self):
+        return "testing...", 404
+
+    def post(self):
+        args = parser.parse_args()
+        text = args['text']
+        analyzer = SentimentIntensityAnalyzer()
+        vs = analyzer.polarity_scores(text)
+        if vs['neu'] > 0.85:
+          #content was approved -> post to database
+          return vs, 200
+        else:
+          #content rejected -> inform user
+          return "does not meet neutrality requirement", 404
+
+api.add_resource(SentimentAnalysis, "/api/v1/")
 
 if __name__ == "__main__":
-  app.run()
+  app.run(host='0.0.0.0', port=5444 ,debug=True)
+  # app.run()
+
+
--- resources/run_vader.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-
-class runVader(Resource):
-    def get(self, content):
-        if content is not None:
-            return "Content Posted ...", 200
-        return "Content was invalid ...", 404
